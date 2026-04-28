3f3d64855d8e451fe65ed6253d7497604ff463ff
beningram7 <jingra78@students.kennesaw.edu>
2026-04-27 23:46:57 -0400

    Add load_data and save_data functions

--- passwordmanager.py
+++ passwordmanager.py
@@ -11,3 +11,14 @@ class Credential:
     service: str
     username: str
     password: str
+def load_data():
+    try:
+        with open(DB_FILE, "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return []
+
+
+def save_data(data):
+    with open(DB_FILE, "w") as f:
+        json.dump(data, f, indent=4)
