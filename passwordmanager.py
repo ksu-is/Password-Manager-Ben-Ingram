fa133233e831b6daefd05dd1345eae81557c386e
beningram7 <jingra78@students.kennesaw.edu>
2026-04-27 23:38:17 -0400

    Add Credential dataclass and import statements

--- passwordmanager.py
+++ passwordmanager.py
@@ -1,1 +1,13 @@
+import json
+import os
+from dataclasses import dataclass, asdict
+from getpass import getpass
 
+DB_FILE = "vault.json"
+
+
+@dataclass
+class Credential:
+    service: str
+    username: str
+    password: str
